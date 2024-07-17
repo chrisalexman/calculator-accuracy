939d86cecfc583c7c00a1ed310eba6837fbe3dd3
Christopher Alexman <christopheralexman@gmail.com>
2024-07-16 23:57:17 -0700

    Add functions to dictionary, reduces number of calculations

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 '''
-
 Calculating the accuracy of the TI-36X Pro calculator using Taylor & Maclaurin series expansions.
 
 NOTE: constraint, calculator can represent max of 9 digits to the right of the decimal
@@ -9,13 +8,23 @@ Functions to test:
 - trigonometric (sin x)
 
 https://en.wikipedia.org/wiki/Taylor_series
-
 '''
 
 import math
 
 
-def calculate_exponential():
+# get expected sum for exponential function
+def get_expected_exponential(x):
+    return round(math.exp(x), 9)
+
+
+# get expected sum for sine function
+def get_expected_sine(x):
+    return round(math.sin(x), 9)
+
+
+# approximate the sum of the exponential function
+def calculate_exponential(x, loop):
 
     '''
     exponential function e^x
@@ -23,37 +32,20 @@ def calculate_exponential():
     Maclaurin series:
         e^x = x^n / n! = 1 + x + x^2/2! + x^3/3! + ...
 
-    for this test, x = 2
-
     --------------------------------------
     | TI-36X | e^2         | 7.389056099 |
     | Python | math.exp(2) | 7.389056099 |
     --------------------------------------
     '''
 
-    x = 2
-    expected = round(math.exp(x), 9)
+    numerator = x ** loop
+    denominator = math.factorial(loop)
 
-    num_tries = 0
-    loops = 99
-    result = 0
-
-    for loop in range(loops):
-
-        numerator = x ** loop
-        denominator = math.factorial(loop)
-
-        result += numerator / denominator
-        rounded_result = round(result, 9)
-
-        num_tries += 1
-
-        if(rounded_result == expected):
-            print(f'exponential function e^x:\nneeded {num_tries} terms to reach result of {rounded_result}')
-            break
+    return numerator / denominator
 
 
-def calculate_sine():
+# approximate the sum of the sine function
+def calculate_sine(x, loop):
 
     '''
     trigonometric function sin(x)
@@ -61,36 +53,56 @@ def calculate_sine():
     Maclaurin series:
         sin(x) = [(-1^n) * x^(2n + 1)] / (2n + 1)! = x - (x^3)/3! + (x^5)/5! - ...
 
-    for this test, x = 2
-
     --------------------------------------
     | TI-36X | sin(2)      | 0.909297427 |
     | Python | math.sin(2) | 0.909297427 |
     --------------------------------------
     '''
+    
+    numerator = ((-1) ** loop) * (x ** (2 * loop + 1))
+    denominator = math.factorial(2 * loop + 1)
+
+    return numerator / denominator
+
+
+# dictionary of functions to test
+functions_to_test = {
+    'exponential': {
+        'expected'  : get_expected_exponential,
+        'calculate' : calculate_exponential
+    },
+    'sine': {
+        'expected'  : get_expected_sine,
+        'calculate' : calculate_sine
+    }
+}
+
+
+# iterate through dictionary of functions to calculate the accuracy of the TI-36X Pro calculator
+def calculate_accuracies():
 
     x = 2
-    expected = round(math.sin(x), 9)
-
-    num_tries = 0
     loops = 99
-    result = 0
+    num_tries = 0
+    result_sum = 0.0
 
-    for loop in range(loops):
+    for key, value in functions_to_test.items():
 
-        numerator = ((-1) ** loop) * (x ** (2 * loop + 1))
-        denominator = math.factorial(2 * loop + 1)
+        expected = value['expected'](x)
 
-        result += numerator / denominator
-        rounded_result = round(result, 9)
+        for loop in range(loops):
 
-        num_tries += 1
+            result_sum += value['calculate'](x, loop)
+            rounded_result = round(result_sum, 9)
+            num_tries += 1
 
-        if(rounded_result == expected):
-            print(f'\ntrig function sin(x):\nneeded {num_tries} terms to reach result of {rounded_result}')
-            break
+            if(rounded_result == expected):
+                print(f'{key} function:\nneeded {num_tries} terms to reach result of {rounded_result}\n')
+                break
+        
+        result_sum = 0.0
+        num_tries = 0
 
 
 if __name__ == '__main__':
-    calculate_exponential()
-    calculate_sine()
+    calculate_accuracies()
